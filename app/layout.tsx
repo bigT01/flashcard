02e3bbb7cbe274e764440a4ea1b981f39323ac5f
tan <tanat8511@gmail.com>
2026-01-07 19:52:48 +0100

    feat: mobile feature removing

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+<meta name="apple-mobile-web-app-title" content="Flashcards" />
+
+
+<meta
+  name="viewport"
+  content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+/>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
